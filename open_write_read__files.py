9542e8bc2385c0ea0ba6f9f5fb53d1dd5c9492ca
Dmitriy Maslennikov <dimych1601@gmail.com>
2025-01-21 15:56:23 +0300

    Task_1

--- open_write_read__files.py
+++ open_write_read__files.py
@@ -1,1 +1,15 @@
-print("HELLO!!!")
+from pprint import pprint
+
+with open('recipes.txt', encoding='UTF-8') as file:
+    cook_book = {}
+    for line in file:
+        recepie_name = line.strip()
+        ingredients_count = file.readline()
+        ingredients = []
+        for p in range(int(ingredients_count)):
+            recepie = file.readline().strip().split(' | ')
+            product, quantity, word = recepie
+            ingredients.append({'ingredient_name': product, 'quantity': quantity, 'measure': word})
+        file.readline()
+        cook_book[recepie_name] = ingredients
+pprint(cook_book)
