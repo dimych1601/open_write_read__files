99f8799272deaedb9164d4e5fde56a96a71cb82d
Dmitriy Maslennikov <dimych1601@gmail.com>
2025-01-22 16:59:05 +0300

    Task_2

--- open_write_read__files.py
+++ open_write_read__files.py
@@ -1,6 +1,8 @@
 from pprint import pprint
 
-with open('recipes.txt', encoding='UTF-8') as file:
+# Задание №1
+
+with open('recipes.txt', 'r', encoding='UTF-8') as file:
     cook_book = {}
     for line in file:
         recepie_name = line.strip()
@@ -12,4 +14,21 @@ with open('recipes.txt', encoding='UTF-8') as file:
             ingredients.append({'ingredient_name': product, 'quantity': quantity, 'measure': word})
         file.readline()
         cook_book[recepie_name] = ingredients
+
 pprint(cook_book)
+
+# Задание №2
+
+def get_shop_list_by_dishes(dishes, person_count):
+    result = {}
+    for dish in dishes:
+        if dish in cook_book:
+            for ingredient in cook_book[dish]:
+                if ingredient['ingredient_name'] in result:
+                    result[ingredient['ingredient_name']]['quantity'] += int(ingredient['quantity']) * person_count
+                else:
+                    result[ingredient['ingredient_name']] = {'measure': ingredient['measure'],'quantity': (int(ingredient['quantity']) * person_count)}
+        else:
+            print('Такого блюда нет в книге')
+    pprint(result)
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
